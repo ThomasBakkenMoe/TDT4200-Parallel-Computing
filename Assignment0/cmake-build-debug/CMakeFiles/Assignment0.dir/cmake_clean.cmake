59559039c9cb91e9ea7f8cb9c012f59f0fb9f5f2
ThomasBakkenMoe <thomasbakkenmoe@gmail.com>
2021-09-08 12:34:25 +0200

    Initial bulk commit

---
+++ Assignment0/cmake-build-debug/CMakeFiles/Assignment0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Assignment0"
+  "Assignment0.pdb"
+  "CMakeFiles/Assignment0.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
